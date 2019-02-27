421a557a1d6e3f44a4a95f55fd011c1d89a8f059
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2019-02-27 10:01:52 +0100

    optimize code for Bliss

--- R/Blissindependence.R
+++ R/Blissindependence.R
@@ -3,39 +3,32 @@
 #' This function returns fractional response levels for when these are based on
 #' Bliss Independence Model.
 #' 
-#' @param doseInput Dose-response dataframe containing \code{"d1"} and
-#'   \code{"d2"} columns
-#' @param parmInput Numeric vector or list with appropriately named
-#'   parameter inputs. Typically, it will be coefficients from a
-#'   \code{MarginalFit} object.
-#' @param ... Further arguments that are currently unused
+#' @inheritParams generalizedLoewe
 
-Blissindependence <- function(doseInput, parmInput, ...){
+Blissindependence <- function(doseInput, parmInput, ...) {
   
   pars <- parmInput
-  #We need the percentage value between 0 and 1 for Bliss estimated effect
-  Hilleq <- function(dose, b, logEC50) {
-    Hilleqread <- 1/(1 + (exp(logEC50)/dose)^(abs(b)))
-  }
+
+  increasing <- pars["m1"] >= pars["b"] && pars["m2"] >= pars["b"]
+  decreasing <- pars["m1"] < pars["b"] && pars["m2"] < pars["b"]
   
-  #calculate prediction mono and rescale to max upper for percentage
-  pred1 <- Hilleq(doseInput[, "d1"], pars["h1"], pars["e1"])
-  pred1 <- pred1*abs(pars["m1"]-pars["b"])/max(abs(pars["m1"]-pars["b"]), abs(pars["m2"]-pars["b"]))
-  pred2 <- Hilleq(doseInput[, "d2"], pars["h2"], pars["e2"])
-  pred2 <- pred2*abs(pars["m2"]-pars["b"])/max(abs(pars["m1"]-pars["b"]), abs(pars["m2"]-pars["b"]))
+  ## If agonist and antagonist, give an error
+  if (!(increasing || decreasing)) {
+    stop("Bliss independence does not work for diverging marginal curves.")
+  }  
   
-  #Bliss independence combination
-  applyfunction <- function(param) {
-    #prediction value in percentage
-    predcombo <- param[1] + param[2] - param[1]*param[2]
-    #determine whether curves are rising or lowering
-    rise = 1
-    if (param[3] > param[4] & param[3] > param[5]) {
-      rise = -1
-    }
-    predcombo <- param[3] + rise*max(abs(param[4]-param[3]), abs(param[5]-param[3]))*predcombo
-    return(predcombo)
-  }
+  # Calculate prediction mono and rescale to max upper for percentage
+  maxRange <- max(abs(pars["m1"]-pars["b"]), abs(pars["m2"]-pars["b"]))
+  pred1 <- L4(doseInput[["d1"]], b = pars["h1"], logEC50 = pars["e1"], L = 0, U = 1) * 
+      abs(pars["m1"]-pars["b"]) / maxRange
+  pred2 <- L4(doseInput[["d2"]], b = pars["h2"], logEC50 = pars["e2"], L = 0, U = 1) *
+      abs(pars["m2"]-pars["b"]) / maxRange
   
-  apply(cbind(pred1, pred2, pars["b"], pars["m1"], pars["m2"]), 1, applyfunction)
+  # Bliss independence combination - prediction value in percentage
+  bliss <- pred1 + pred2 - pred1*pred2
+  
+  # rescale
+  direction <- 1*increasing - 1*decreasing
+  pars["b"] + direction * maxRange * bliss
+ 
 }
