71624b793954e3e52b5b8abc0fec709acce37bb0
shawinke <stijn.hawinkel@ugent.be>
2020-08-03 17:01:37 +0200

    Bug in loewe2

--- R/harbronLoewe.R
+++ R/harbronLoewe.R
@@ -7,22 +7,24 @@ invL4deriv <- function(y, h, b, m) {
 }
 
 # calculation of d/D in the additivity/synergy equation
-doseRatio <- function(response, d, h, b, m, expe, lower, upper, exVal) {
+doseRatio <- function(response, d, h, b, m, expe, lower, upper) {
   # In case of different asymptotes, response can be outside of [lower, upper]
   # range, this needs to be handled separately (otherwise formula gives NaN)
   if(!d){
     return(0)
-  } else if(response < lower || response > upper){
-    return(exVal)
+  } else if(response <= lower){
+    return(if(b < m) 0 else Inf)
+  } else if (response >= upper){
+    return(if(b < m) Inf else 0)
   } else {
     return(d/(invL4(response, h, b, m)*expe))
   }
 }
-doseRatioGr = function(response, d, h, b, m, expe, lower, upper, exVal){
+doseRatioGr = function(response, d, h, b, m, expe, lower, upper){
   if(!d || response < lower || response > upper){
     return(0)
   } else {
-    return(d/expe* (-invL4deriv(response, h, b, m)))#/invL4(response, h, b, m)^2
+    return(d/expe* (-invL4deriv(response, h, b, m)))
   }
 }
 #' Alternative Loewe generalization
@@ -45,22 +47,24 @@ harbronLoewe <- function (doseInput, parmInput, asymptotes = 2,
   if (!(increasing || decreasing)) {
     stop("Alternative Loewe generalization does not work for diverging marginal curves.")
   }
-  exVal = if (parm["b"] < parm["m1"]) 0 else Inf
   expE1 = exp(parm["e1"]);expE2 = exp(parm["e2"])
   lower1 <- min(parm[c("b", "m1")]);upper1 <- max(parm[c("b", "m1")])
   lower2 <- min(parm[c("b", "m2")]);upper2 <- max(parm[c("b", "m2")])
+  lower = min(lower1, lower2); upper = max(upper1, upper2)
   solver <- function(dose, par) {
     fun0 <- function(y) {
-      res <- doseRatio(y, dose[1], par["h1"], par["b"], par["m1"], expE1, lower1, upper1, exVal) +
-          doseRatio(y, dose[2], par["h2"], par["b"], par["m2"], expE2, lower2, upper2, exVal) - 1
+      res <- doseRatio(y, dose[1], par["h1"], par["b"], par["m1"], expE1, lower1, upper1) +
+          doseRatio(y, dose[2], par["h2"], par["b"], par["m2"], expE2, lower2, upper2) - 1
       if(is.finite(res)) res else 1
     }
     gr0 = function(y){
-      doseRatioGr(y, dose[1], par["h1"], par["b"], par["m1"], expE1, lower1, upper1, exVal) +
-        doseRatioGr(y, dose[2], par["h2"], par["b"], par["m2"], expE2, lower2, upper2, exVal)
+      doseRatioGr(y, dose[1], par["h1"], par["b"], par["m1"], expE1, lower1, upper1) +
+        doseRatioGr(y, dose[2], par["h2"], par["b"], par["m2"], expE2, lower2, upper2)
     }
-    nleqslv(fn = fun0, x = dose[3], jac = gr0,
+    out = nleqslv(fn = fun0, x = dose[3], jac = gr0,
             control = list(ftol = .Machine$double.eps))$x
+    out = if(out <= lower) {lower} else if(out >= upper) {upper} else {out}
+    return(out)
   }
 
   ## Remove observations where both drugs are dosed at zero
