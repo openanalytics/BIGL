5fc1f7f5e2ec681c222c6cca0d2fc285de8b1137
shawinke <stijn.hawinkel@ugent.be>
2020-08-05 10:50:18 +0200

    Changed the tests for confint

--- tests/testthat/test_confint.R
+++ tests/testthat/test_confint.R
@@ -10,10 +10,6 @@ test_that('confidence intervals', {
     expect_true(all(with(confInt, offAxis[,1] < offAxis[,2])))
 
     # Single measure confidence intervals
-    expect_true(all(confInt$single$confIntMeanEffect[,1] <
+    expect_true(all(confInt$single$confIntMeanEffect[,1] <=
                         confInt$single$confIntMeanEffect[, 2]))
-    expect_true(all(confInt$single$confIntMeanEffect[,1] <
-                        confInt$single$meanEffect))
-    expect_true(all(confInt$single$confIntMeanEffect[,2] >
-                    confInt$single$meanEffect))
 })
