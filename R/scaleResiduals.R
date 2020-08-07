2dcf193fe0d7aeb8fd8e03efc6b37d621ef556ae
shawinke <stijn.hawinkel@ugent.be>
2020-08-07 14:00:37 +0200

    Resample residuals

---
+++ R/scaleResiduals.R
@@ -0,0 +1,3 @@
+#' Functions for scaling, and rescaling residuals
+#' @details Residuals are calculated with respect to the average observation on
+#' the off-axis point, so replicates are required!
