589e0f2c84843fff41ad429dea1ff1f010b5ebd0
shawinke <stijn.hawinkel@ugent.be>
2020-08-07 16:41:09 +0200

    complete docu

--- R/scaleResids.R
+++ R/scaleResids.R
@@ -1,22 +1,28 @@
 #' Functions for scaling, and rescaling residuals
 #' @details Residuals are calculated with respect to the average observation on
 #' the off-axis point, so replicates are required!
-#' @param sampling_errors,scaledResids A vector of raw or scaled residuals
+#' @param sampling_errors A vector of raw  residuals
 #' @param means a vector of means
 #' @param model coefficients of a fitted mean-variance trend
 scaleResids = function(sampling_errors, means, model){
     predVar = predictVar(means, model)
     sampling_errors/sqrt(predVar)
 }
+#' Backscale residuals
+#' @param scaledResids scaled residuals
+#' @inheritParams scaleResids
 backscaleResids = function(scaledResids, means, model){
     predVar = predictVar(means, model)
     scaledResids*sqrt(predVar)
 }
+#'Predict variance
+#' @inheritParams scaleResids
 predictVar = function(means, model){
     predVar = model[1] + model[2]*means
 }
 #' Add residuals by adding to mean effects
 #' @param method a variance method, see \code{\link{fitSurface}}
+#' @inheritParams scaleResids
 addResids = function(means, sampling_errors, method, model){
     if(method %in% c("equal", "unequal")){
         return(means + sample(sampling_errors, replace = TRUE))
